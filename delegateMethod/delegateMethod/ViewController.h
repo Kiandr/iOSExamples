f8a142458f2b22dfaf3f10ad7a2cebe07d02e2e1
Kian Davoudi-Rad <kdr@Kians-MacBook-Pro.local>
2017-03-15 18:45:57 -0700

    adding delegate example here

---
+++ delegateMethod/delegateMethod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  delegateMethod
+//
+//  Created by Kian Davoudi-Rad on 2017-03-15.
+//  Copyright © 2017 Kian Davoudi-Rad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
