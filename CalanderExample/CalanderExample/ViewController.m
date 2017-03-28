ff9c1b92dc006a4a0e45ac1fbb939dff4eb77d44
Kian Davoudi-Rad <kdr@Kians-MacBook-Pro.local>
2017-03-28 15:36:35 -0700

    some changes about EventKit

--- CalanderExample/CalanderExample/ViewController.m
+++ CalanderExample/CalanderExample/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <EventKit/EventKit.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // Functio Description:
+    // Get all events in for a certain day.
+
+//    EKEventStore *eventStore = [[EKEventStore alloc] init];
+    EKEventStore *store = [[EKEventStore alloc] init];
+    EKEventStore *store = [[EKEventStore alloc] initWithAccessToEntityTypes:EKEntityMaskEvent];
+
+//    NSCalendar *calendar = [NSCalendar currentCalendar];
+//
+//    // Create the start date components
+//    NSDateComponents *oneDayAgoComponents = [[NSDateComponents alloc] init];
+//    oneDayAgoComponents.day = -1;
+//    NSDate *oneDayAgo = [calendar dateByAddingComponents:oneDayAgoComponents
+//                                                  toDate:[NSDate date]
+//                                                 options:0];
+//
+//    // Create the end date components
+//    NSDateComponents *oneYearFromNowComponents = [[NSDateComponents alloc] init];
+//    oneYearFromNowComponents.year = 1;
+//    NSDate *oneYearFromNow = [calendar dateByAddingComponents:oneYearFromNowComponents
+//                                                       toDate:[NSDate date]
+//                                                      options:0];
+//
+//    // Create the predicate from the event store's instance method
+//    NSPredicate *predicate = [store predicateForEventsWithStartDate:oneDayAgo
+//                                                            endDate:oneYearFromNow
+//                                                          calendars:nil];
+//
+//    // Fetch all events that match the predicate
+//    NSArray *events = [store eventsMatchingPredicate:predicate];
+
+
+
 }
 
 
